67ac9872f20de13c5d3f36a6a567ef76c099b10c
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-03-08 19:08:17 -0500

    avancement page 3

--- script/main.js
+++ script/main.js
@@ -1,27 +1,27 @@
-window.onload = function () {
+// window.onload = function () {
 
 
-    // Open modal when trigger button is clicked
-    document.getElementById('modal-trigger').addEventListener('click', function () {
-        console.log('test');
-        document.getElementById('modal').classList.add('show');
-    });
+//     // Open modal when trigger button is clicked
+//     document.getElementById('modal-trigger').addEventListener('click', function () {
+//         console.log('test');
+//         document.getElementById('modal').classList.add('show');
+//     });
 
-    // Close modal when clicking outside the modal
-    document.addEventListener('click', function (event) {
-        var modal = document.getElementById('modal');
-        if (event.target.closest('#modal') === null && event.target.closest('#modal-trigger') === null) {
-            modal.classList.remove('show');
-        }
-    });
+//     // Close modal when clicking outside the modal
+//     document.addEventListener('click', function (event) {
+//         var modal = document.getElementById('modal');
+//         if (event.target.closest('#modal') === null && event.target.closest('#modal-trigger') === null) {
+//             modal.classList.remove('show');
+//         }
+//     });
 
-    // Submit form and close modal
-    document.getElementById('booking-form').addEventListener('submit', function (event) {
-        event.preventDefault();
-        // Handle form submission logic here
+//     // Submit form and close modal
+//     document.getElementById('booking-form').addEventListener('submit', function (event) {
+//         event.preventDefault();
+//         // Handle form submission logic here
 
-        // Close modal
-        document.getElementById('modal').classList.remove('show');
-    });
+//         // Close modal
+//         document.getElementById('modal').classList.remove('show');
+//     });
 
-};
+// };
